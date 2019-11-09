8e7f6cdb8c897bfe7d679e91ba33b6302e444558
srcolinas <sebastian.rodriguez@landing.ai>
2019-11-09 17:20:52 -0500

    -.-

--- logistic_regression/preprocessing.py
+++ logistic_regression/preprocessing.py
@@ -14,7 +14,7 @@ def test_case2(X):
 def test_case3(X):
     """
     This function implements the required preprocessing to be able to solve
-    dataset number 2 of the classification toy datasets using logistic
+    dataset number 3 of the classification toy datasets using logistic
     regression.
     """
     return (X[:, 0] * X[:, 1])[:, None]
@@ -22,7 +22,7 @@ def test_case3(X):
 def test_case4(X):
     """
     This function implements the required preprocessing to be able to solve
-    dataset number 2 of the classification toy datasets using logistic
+    dataset number 4 of the classification toy datasets using logistic
     regression.
     """
     return X
@@ -30,7 +30,7 @@ def test_case4(X):
 def test_case5(X):
     """
     This function implements the required preprocessing to be able to solve
-    dataset number 2 of the classification toy datasets using logistic
+    dataset number 5 of the classification toy datasets using logistic
     regression.
     """
     return X
